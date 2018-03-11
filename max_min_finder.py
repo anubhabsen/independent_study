a9eab8fabd5c4913836d01383cbe39fc52823540
Anubhab Sen <anubhabsen@gmail.com>
2018-03-12 00:20:56 +0530

    updated code

--- max_min_finder.py
+++ max_min_finder.py
@@ -10,14 +10,16 @@ corr = data[0]
 p_vals = data[1]
 
 
-print(p_vals.shape)
+fields = ["Results Pitch", "Results Time", "Age", "Gender","WAIS_vocabularyRP","WAIS_vocabularySP","WAIS_similaritiesRP","WAIS_similaritiesSP","WAIS_informationRP","WAIS_informationSP","WAIS_picturecompletionRP","WAIS_picturecompletionSP","WAIS_digitsymbolcodingRP","WAIS_digitsymbolcodingSP","WAIS_blockdesignRP","WAIS_blockdesignSP","WAIS_matrixreasoningRP","WAIS_matrixreasoningSP","WAIS_symbolsearchRP","WAIS_symbolsearchSP","WAIS_VCI_verbal_comprehension_index","WAIS_POI_perceptual_organization_index","WAIS_PSI_processing_speed_index","WMS_letternumbersequenceRP","WMS_letternumbersequenceSP","WMS_spatialspanRP","WMS_spatialspanSP","WMS_word_lists1RP","WMS_word_lists1SP","WMS_word_lists2RP","WMS_word_lists2SP","WMS_wordlists_recognitionRP","WMS_wordlists_recognitionSP","WMS_workingmemoryindex","TrailMakingA_time_in_seconds","TrailMakingA_percentile","TrailMakingA_numberoferrors","TrailMakingB_time_in_seconds","TrailMakingB_percentile","TrailMakingB_numberoferrors","TrailMaking_difference_percentile","TrailMaking_ratiopercentile","Stroop_word_reading_time","Stroop_word_reading_no_of_errors","Stroop_colournaming_time","Stroop_colournaming_nooferrors","Stroop_interference_time","Stroop_interference_nooferrors"]
+counter = 0
 
+# Correlation
 # for i in range(48):
 # 	for j in range(48):
 # 		if i == j:
 # 			corr[i, j] = 0
 
-# for counter in range(20):
+# while counter < 10:
 # 	max = 0
 # 	for i in range(48):
 # 		for j in range(48):
@@ -26,21 +28,19 @@ print(p_vals.shape)
 # 				temp_i = i
 # 				temp_j = j
 
-# 	if temp_j > temp_i:
+# 	if temp_j > temp_i and abs(temp_j - temp_i) > 1:
 # 		indices.append((temp_i, temp_j))
+# 		counter += 1
 # 	corr[temp_i, temp_j] = 0
 
-# print(indices)
 
-
-
-
+# The other thing
 # for i in range(48):
 # 	for j in range(48):
 # 		if i == j:
 # 			p_vals[i, j] = 100
 
-# for counter in range(20):
+# while counter < 10:
 # 	min = 9999999
 # 	for i in range(48):
 # 		for j in range(48):
@@ -49,9 +49,11 @@ print(p_vals.shape)
 # 				temp_i = i
 # 				temp_j = j
 
-# 	if temp_j > temp_i:
+# 	if temp_j > temp_i and abs(temp_i - temp_j) > 1:
 # 		indices.append((temp_i, temp_j))
+# 		counter += 1
 # 	p_vals[temp_i, temp_j] = 100
 
-# print(indices)
-# print(p_vals)
+
+for indexes in indices:
+	print(fields[indexes[0]],"and", fields[indexes[1]])
