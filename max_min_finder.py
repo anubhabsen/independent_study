8e7cc9a604eadaef5ecb46a87874f2785206635d
Anubhab Sen <anubhabsen@gmail.com>
2018-04-06 17:16:25 +0530

    new fields

--- max_min_finder.py
+++ max_min_finder.py
@@ -1,7 +1,7 @@
 import pickle
 import numpy as np
 
-with open('mats.pkl', 'rb') as f:
+with open('mats_new.pkl', 'rb') as f:
     data = pickle.load(f)
 
 indices = []
@@ -10,58 +10,58 @@ corr = data[0]
 p_vals = data[1]
 
 # Convert all the values to positives
-for i in range(48):
-	for j in range(48):
+for i in range(55):
+	for j in range(55):
 		corr[i, j] = abs(corr[i, j])
 
-for i in range(48):
-	for j in range(48):
+for i in range(55):
+	for j in range(55):
 		p_vals[i, j] = abs(p_vals[i, j])
 
 
-fields = ["Results Pitch", "Results Time", "Age", "Gender","WAIS_vocabularyRP","WAIS_vocabularySP","WAIS_similaritiesRP","WAIS_similaritiesSP","WAIS_informationRP","WAIS_informationSP","WAIS_picturecompletionRP","WAIS_picturecompletionSP","WAIS_digitsymbolcodingRP","WAIS_digitsymbolcodingSP","WAIS_blockdesignRP","WAIS_blockdesignSP","WAIS_matrixreasoningRP","WAIS_matrixreasoningSP","WAIS_symbolsearchRP","WAIS_symbolsearchSP","WAIS_VCI_verbal_comprehension_index","WAIS_POI_perceptual_organization_index","WAIS_PSI_processing_speed_index","WMS_letternumbersequenceRP","WMS_letternumbersequenceSP","WMS_spatialspanRP","WMS_spatialspanSP","WMS_word_lists1RP","WMS_word_lists1SP","WMS_word_lists2RP","WMS_word_lists2SP","WMS_wordlists_recognitionRP","WMS_wordlists_recognitionSP","WMS_workingmemoryindex","TrailMakingA_time_in_seconds","TrailMakingA_percentile","TrailMakingA_numberoferrors","TrailMakingB_time_in_seconds","TrailMakingB_percentile","TrailMakingB_numberoferrors","TrailMaking_difference_percentile","TrailMaking_ratiopercentile","Stroop_word_reading_time","Stroop_word_reading_no_of_errors","Stroop_colournaming_time","Stroop_colournaming_nooferrors","Stroop_interference_time","Stroop_interference_nooferrors"]
+fields = ["Results Pitch", "Results Time", "Age", "Gender","WAIS_vocabularyRP","WAIS_vocabularySP","WAIS_similaritiesRP","WAIS_similaritiesSP","WAIS_informationRP","WAIS_informationSP","WAIS_picturecompletionRP","WAIS_picturecompletionSP","WAIS_digitsymbolcodingRP","WAIS_digitsymbolcodingSP","WAIS_blockdesignRP","WAIS_blockdesignSP","WAIS_matrixreasoningRP","WAIS_matrixreasoningSP","WAIS_symbolsearchRP","WAIS_symbolsearchSP","WAIS_VCI_verbal_comprehension_index","WAIS_POI_perceptual_organization_index","WAIS_PSI_processing_speed_index","WMS_letternumbersequenceRP","WMS_letternumbersequenceSP","WMS_spatialspanRP","WMS_spatialspanSP","WMS_word_lists1RP","WMS_word_lists1SP","WMS_word_lists2RP","WMS_word_lists2SP","WMS_wordlists_recognitionRP","WMS_wordlists_recognitionSP","WMS_workingmemoryindex","TrailMakingA_time_in_seconds","TrailMakingA_percentile","TrailMakingA_numberoferrors","TrailMakingB_time_in_seconds","TrailMakingB_percentile","TrailMakingB_numberoferrors","TrailMaking_difference_percentile","TrailMaking_ratiopercentile","Stroop_word_reading_time","Stroop_word_reading_no_of_errors","Stroop_colournaming_time","Stroop_colournaming_nooferrors","Stroop_interference_time","Stroop_interference_nooferrors", "IRI_PT","IRI_FS","IRI_EC","IRI_PD","mbea_block1_scale","mbea_block2_off_beat","mbea_block3_out_of_key"]
 counter = 0
 
 # Correlation
-# for i in range(48):
-# 	for j in range(48):
-# 		if i == j:
-# 			corr[i, j] = 0
-
-# while counter < 10:
-# 	max = 0
-# 	for i in range(48):
-# 		for j in range(48):
-# 			if corr[i, j] > max and (i == 1 or j == 1):
-# 				max = corr[i, j]
-# 				temp_i = i
-# 				temp_j = j
-
-# 	if temp_j > temp_i and abs(temp_j - temp_i) > 1:
-# 		indices.append((temp_i, temp_j))
-# 		counter += 1
-# 	corr[temp_i, temp_j] = 0
-
-
-# The p values
 for i in range(48):
 	for j in range(48):
 		if i == j:
-			p_vals[i, j] = 100
+			corr[i, j] = 0
 
 while counter < 10:
-	min = 9999999
+	max = 0
 	for i in range(48):
 		for j in range(48):
-			if p_vals[i, j] < min and (i == 1 or j == 1):
-				min = p_vals[i, j]
+			if corr[i, j] > max and (i == 1 or j == 1):
+				max = corr[i, j]
 				temp_i = i
 				temp_j = j
 
-	if temp_j > temp_i and abs(temp_i - temp_j) > 1:
+	if temp_j > temp_i and abs(temp_j - temp_i) > 1:
 		indices.append((temp_i, temp_j))
 		counter += 1
-	p_vals[temp_i, temp_j] = 100
+	corr[temp_i, temp_j] = 0
+
+
+# The p values
+# for i in range(55):
+# 	for j in range(55):
+# 		if i == j:
+# 			p_vals[i, j] = 100
+
+# while counter < 10:
+# 	min = 9999999
+# 	for i in range(55):
+# 		for j in range(55):
+# 			if p_vals[i, j] < min and (i == 1 or j == 1):
+# 				min = p_vals[i, j]
+# 				temp_i = i
+# 				temp_j = j
+
+# 	if temp_j > temp_i and abs(temp_i - temp_j) > 1:
+# 		indices.append((temp_i, temp_j))
+# 		counter += 1
+# 	p_vals[temp_i, temp_j] = 100
 
 
 for indexes in indices:
